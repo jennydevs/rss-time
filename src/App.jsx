bcd0590b4013959a4d704437f5dfa62aa20a0150
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-02-03 13:21:43 -0500

    basic reading in xml and mapping to visible components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Article({item}) {
+    return (
+        <div>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+            <p>{item.link}</p>
+            <a href={item.link}>{item.link}</a>
+        </div>
+    );
 }
 
-export default App
+
+function parseData(doc) {
+    var items = doc.getElementsByTagName('item');
+
+    const tags = ['title', 'link', 'description', 'guid', 'pubDate'];
+
+    var items_list = [];
+    for (let i = 0; i < items.length; i++) {
+        var content = {};
+        for (let j = 0; j < tags.length; j++) {
+            let tag_items = items[i].getElementsByTagName(tags[j]);
+            if (tag_items.length != 0) {
+                content[tags[j]] = tag_items[0].textContent;
+            } 
+            else {
+                content[tags[j]] = null;
+            }
+        }
+
+        items_list.push(content);
+    }
+
+    return (items_list.map((item) => <Article key={item.guid} item={item}/>));
+}
+
+
+function App(){
+    const [rss_articles, setArticles] = useState([]); 
+
+    useEffect(() => {
+        async function getRSSFeed() {
+            await fetch("./rss_examples/...")
+            .then((response)=> {
+                return response.text();
+            })
+            .then((rss_feed) => {
+                var parser = new DOMParser();
+                var xml_doc = parser.parseFromString(rss_feed, "text/xml");
+
+                setArticles(parseData(xml_doc))
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+        }
+
+        getRSSFeed();
+    }, []);
+
+    return(
+        <>
+            {
+                !rss_articles ? 
+                    <p>Loading...</p> :
+                    <div>
+                        {rss_articles}
+                    </div>
+            }
+        </>
+    );
+}
+
+
+export default App;
