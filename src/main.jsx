9d1c3111ae19e13c0bb78ff767bc769d7d19d2e1
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-02-08 21:52:16 -0500

    create routes to the two pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+import Home from './pages/Home.jsx';
+import Settings from './pages/Settings.jsx';
+import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/settings',
+    element: <Settings/>
+  }
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
