4039773ae6b69e4fbf7ca951960a6dc8e5bcc8f8
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-02-10 23:20:05 -0500

    add error page, change feedlist

--- src/components/FeedList.jsx
+++ src/components/FeedList.jsx
@@ -5,10 +5,18 @@ function FeedList({feed_links}) {
     return (
         <>
             {
-                feed_links.length == 0 ? <p>Loading...</p> :
-                    feed_links.map((feed_link) => 
-                        <li className='feed-info'><FeedBlob key={0} feed_info={feed_link}/></li>
-            )}
+                feed_links.length == 0 ? <p>No feeds yet!</p> :
+                    <ul className='feed-list'>
+                    {feed_links.map((feed_link) => 
+                        <li className='feed-info'>
+                            <button>Up</button>
+                            <button>down</button>
+                            <FeedBlob key={0} feed_info={feed_link}/>
+                            <button>trash</button>
+                        </li>
+                    )}
+                    </ul>
+            }
         </>
     );
 }
---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+
+
+
+function Error() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default Error;
