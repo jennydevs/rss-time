a676b2d88c7096fe742b0a4eb594e4196e19c7a5
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-02-11 11:30:45 -0500

    add error page from my website

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,20 @@
-
+import { useRouteError, useNavigate } from 'react-router';
 
 
 function Error() {
+    const error = useRouteError();
+    const navigate = useNavigate();
+
     return (
-        <>
-        </>
+        <div className='container' style={{textAlign:'center', paddingBottom:'20px'}}>
+            <div className="content"  style={{textAlign:'center', padding:'0'}}>
+                <h1 style={{paddingTop:'10px'}}>{error.status || 'Error'}</h1>
+                <hr/>
+                <p>{error.statusText || error.message}</p>
+                { console.error(error) }
+                <button  type="button" onClick={() => { navigate(-1) }} className='error-button'>Go back</button>
+            </div>
+        </div>
     );
 }
 
