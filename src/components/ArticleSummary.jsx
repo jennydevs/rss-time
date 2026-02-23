4fea8380b5fc6b5b2dc7393d002f459283a59e47
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-02-23 12:45:11 -0500

    bg for modal

--- src/components/ArticleSummary.jsx
+++ src/components/ArticleSummary.jsx
@@ -2,12 +2,14 @@
 
 function ArticleSummary({setOpened, summary}) {
     return(
-            <div className='modal'>
+        <div className='modal'>
+            <div className='modal-content'>
                 <p>{summary.title}</p>
                 <p>{summary.pubDate}</p>
                 <p>{summary.description}</p>
                 <button onClick={() => {setOpened(false)}}>Close</button>
             </div>
+        </div>
     );
 }
 
